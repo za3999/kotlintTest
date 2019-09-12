4b6732c69030b13739416481d736f3312a155567
zhengcaifu <344984602@qq.com>
2019-09-12 14:29:28 +0800

    Kotlin实现Parcelable

--- app/src/main/java/com/test/kotlin/kotlintest/test/common/test/bean/User.kt
+++ app/src/main/java/com/test/kotlin/kotlintest/test/common/test/bean/User.kt
@@ -1,10 +1,15 @@
 package com.test.kotlin.kotlintest.test.common.test.bean
 
+import android.annotation.SuppressLint
+import android.os.Parcelable
 import android.util.Log
 import com.test.kotlin.kotlintest.test.common.Constants
+import kotlinx.android.parcel.Parcelize
 import kotlin.properties.Delegates
 
-data class User(var name: String, var age: Int) {
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class User(var name: String, var age: Int) : Parcelable {
 
     var temp: String by Delegates.observable("初始值") { prop, old, new ->
         Log.d(Constants.TAG, "旧值：$old -> 新值：$new")
