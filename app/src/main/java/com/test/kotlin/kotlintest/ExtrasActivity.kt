9dc70719391743b99954bd22a50e3b4c7ff377df
zhengcaifu <344984602@qq.com>
2019-09-18 16:18:30 +0800

    ExtrasDelegate+1

--- app/src/main/java/com/test/kotlin/kotlintest/ExtrasActivity.kt
+++ app/src/main/java/com/test/kotlin/kotlintest/ExtrasActivity.kt
@@ -9,13 +9,12 @@ import com.test.kotlin.kotlintest.test.common.test.util.ExtrasDelegate
 
 class ExtrasActivity : AppCompatActivity() {
     private var user: User? by ExtrasDelegate("user", null)
-    private var string: String? by ExtrasDelegate("sring", "test")
+    private var string: String? by ExtrasDelegate("string", "test")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(Constants.TAG, "ExtrasActivity user:$user,string:$string")
         string = "哈哈哈"
-//        intent.putExtra("string","哈哈哈哈哈哈")
         Log.d(Constants.TAG, "ExtrasActivity user:$user,string:$string")
     }
 }
